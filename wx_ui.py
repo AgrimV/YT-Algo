d954e6f26a74dc4c1c9dfe42e40332816bf5187c
AgrimV <agrimvats10@gmail.com>
2020-11-29 15:27:41 +0530

    UI improvisation

--- wx_ui.py
+++ wx_ui.py
@@ -3,6 +3,18 @@ import wx.adv
 
 import yt_api
 
+import platform
+
+
+def assign_icon(icon_name):
+    """
+    Return path to the icon
+    """
+    if platform.system() == 'Linux':
+        return "./icons/" + icon_name
+    else:
+        return "icons\\" + icon_name
+
 
 class Frame(wx.Frame):
 
@@ -29,7 +41,8 @@ class Frame(wx.Frame):
         my_sizer.Add(self.text, 0, wx.ALL, 5)
         panel.SetSizer(my_sizer)
 
-        search = wx.Bitmap("icons\search_reduced.bmp", wx.BITMAP_TYPE_ANY)
+        search = wx.Bitmap(assign_icon("search_reduced.bmp"),
+                           wx.BITMAP_TYPE_ANY)
         button = wx.BitmapButton(panel, id=wx.ID_ANY, bitmap=search,
                                  pos=(417, 28), size=(30, 30))
         button.Bind(wx.EVT_BUTTON, self.onButton)
@@ -40,7 +53,7 @@ class Frame(wx.Frame):
         Send the query to search
         """
         sub_app = wx.App(True)
-        Results(yt_api.search(self.text.GetValue()))
+        Results(yt_api.search(self.text.GetValue()), self.text.GetValue())
         sub_app.MainLoop()
 
 
@@ -48,45 +61,55 @@ class Results(wx.Frame):
 
     """Displaying the results"""
 
-    def __init__(self, results):
+    def __init__(self, results, search_query):
         """Frame for results"""
         wx.Frame.__init__(self, None, pos=wx.DefaultPosition,
                           size=wx.Size(475, 100),
                           style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
-                          wx.CLOSE_BOX | wx.CLIP_CHILDREN, title='Results')
+                          wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+                          title='Results for ' + search_query)
 
         self.results = results
-        for index, result in enumerate(results):
-            print('\n' + str(index + 1) + '.  ' +
-                  result['snippet']['title'] + '\n')
+        if self.results:
+            for index, result in enumerate(results):
+                print('\n' + str(index + 1) + '.  ' +
+                      result['snippet']['title'])
+                print('Published On : ' + result['snippet']['publishedAt'])
+                vid_info = yt_api.video_info(result['id']['videoId'])
+                print('Views : ' + str(vid_info['views']))
+                print('Likes : ' + str(vid_info['likes']), end='\t')
+                print('Dislikes : ' + str(vid_info['dislikes']) + '\n')
 
-        panel = wx.Panel(self)
-        label = wx.StaticText(panel,
-                              label='Enter the number of video to play it')
+            panel = wx.Panel(self)
+            label = wx.StaticText(panel,
+                                  label='Enter the number of video to play it')
 
-        my_sizer = wx.BoxSizer(wx.VERTICAL)
-        my_sizer.Add(label, 0, wx.ALL, 5)
+            my_sizer = wx.BoxSizer(wx.VERTICAL)
+            my_sizer.Add(label, 0, wx.ALL, 5)
 
-        self.text = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,
-                                size=(400, 30))
-        self.text.SetFocus()
-        self.text.Bind(wx.EVT_TEXT_ENTER, self.onButton)
+            self.text = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,
+                                    size=(400, 30))
+            self.text.SetFocus()
+            self.text.Bind(wx.EVT_TEXT_ENTER, self.onButton)
 
-        my_sizer.Add(self.text, 0, wx.ALL, 5)
-        panel.SetSizer(my_sizer)
+            my_sizer.Add(self.text, 0, wx.ALL, 5)
+            panel.SetSizer(my_sizer)
 
-        search = wx.Bitmap("icons\play_reduced.bmp", wx.BITMAP_TYPE_ANY)
-        button = wx.BitmapButton(panel, id=wx.ID_ANY, bitmap=search,
-                                 pos=(417, 28), size=(30, 30))
+            search = wx.Bitmap(assign_icon("play_reduced.bmp"),
+                               wx.BITMAP_TYPE_ANY)
+            button = wx.BitmapButton(panel, id=wx.ID_ANY, bitmap=search,
+                                     pos=(417, 28), size=(30, 30))
 
-        button.Bind(wx.EVT_BUTTON, self.onButton)
-        self.Show()
+            button.Bind(wx.EVT_BUTTON, self.onButton)
+            self.Show()
 
     def onButton(self, event):
         serial = self.text.GetValue()
         for index, result in enumerate(self.results):
             if int(serial) == index + 1:
                 yt_api.redirect(result['id']['videoId'])
+
+        yt_api.redirect('not_available')
 
 
 if __name__ == '__main__':
